4b0bee386d2d1fee3141d84bee724fed297f7c5f
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-08-31 14:49:33 +0200

    Arrow notation

--- aufgabe/VierPunktEins.java
+++ aufgabe/VierPunktEins.java
@@ -1,23 +1,15 @@
 public class VierPunktEins {
     public static void main(String[] args) {
-        int note = 5;
+        int note = 1;
 
         switch (note) {
 
-            case 1:
-                System.out.println("Sehr gut");
-                break;
-            case 2:
-                System.out.println("Gut");
-                break;
-            case 3:
-                System.out.println("erfriedigend");
-                break;
-            case 4:
-                System.out.println("Genügend");
-                break;
-            case 5:
-                System.out.println("nicht Genügend");
+            case 1 -> System.out.println("Sehr gut");
+            case 2 -> System.out.println("Gut");
+            case 3 -> System.out.println("erfriedigend");
+            case 4 -> System.out.println("Genügend");
+            case 5 -> System.out.println("nicht Genügend");
+
         }
     }
 }
