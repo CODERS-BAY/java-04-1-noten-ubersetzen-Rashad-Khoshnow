816ee7a0b07aa38f4b26dd7e0d78d2ee569c7484
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-08-31 14:33:25 +0200

    Autoformatiert

--- aufgabe/VierPunktEins.java
+++ aufgabe/VierPunktEins.java
@@ -1,23 +1,23 @@
 public class VierPunktEins {
     public static void main(String[] args) {
-        int note= 5;
+        int note = 5;
 
         switch (note) {
 
-        case 1:
-            System.out.println("Sehr gut");
-            break;
-        case 2 :
-            System.out.println("Gut");
-            break;
-        case 3 :
-            System.out.println("erfriedigend");
+            case 1:
+                System.out.println("Sehr gut");
                 break;
-        case 4:
-            System.out.println("Genügend");
+            case 2:
+                System.out.println("Gut");
                 break;
-        case 5 :
-            System.out.println("nicht Genügend");
+            case 3:
+                System.out.println("erfriedigend");
+                break;
+            case 4:
+                System.out.println("Genügend");
+                break;
+            case 5:
+                System.out.println("nicht Genügend");
         }
     }
 }
