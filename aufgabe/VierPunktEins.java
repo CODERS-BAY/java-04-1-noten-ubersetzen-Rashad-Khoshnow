ed327898100da77e6528de8c195a2f993e5ed8a2
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-08-31 13:21:56 +0200

    Aufgabe erledigt

---
+++ aufgabe/VierPunktEins.java
@@ -0,0 +1,23 @@
+public class VierPunktEins {
+    public static void main(String[] args) {
+        int note= 5;
+
+        switch (note) {
+
+        case 1:
+            System.out.println("Sehr gut");
+            break;
+        case 2 :
+            System.out.println("Gut");
+            break;
+        case 3 :
+            System.out.println("erfriedigend");
+                break;
+        case 4:
+            System.out.println("Genügend");
+                break;
+        case 5 :
+            System.out.println("nicht Genügend");
+        }
+    }
+}
